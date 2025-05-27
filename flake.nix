4ea3cb38fdc790aa1a51c3b468b1bf580be519e8
Amy Pennucci <apennucci@protonmail.com>
2025-05-27 15:32:42 +0200

    flake: add gjs for testing in console

--- flake.nix
+++ flake.nix
@@ -6,11 +6,16 @@
         with nixpkgs.legacyPackages.x86_64-linux;
         mkShell {
           packages = [
+            just
+
+            # Run from command line
+            gjs
+            clutter
+
+            # Building
             gettext
             glib.dev
             gnumake
-            zip
-            just
           ];
         };
     };
