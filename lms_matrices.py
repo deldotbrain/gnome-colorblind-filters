9ffda88f802c5b92ae251aac67a80c1ba241c983
Amy Pennucci <apennucci@protonmail.com>
2025-05-29 21:22:07 +0200

    lms_matrices: define transforms more scientifically
    Also, use the array-of-arrays np.matrix constructor. Why was I even
    using the string constructor to begin with?

--- lms_matrices.py
+++ lms_matrices.py
@@ -3,42 +3,66 @@
 import numpy as np
 import json
 
-# sRGB definition (https://en.wikipedia.org/wiki/SRGB#Primaries):
-rgb2xyz = np.matrix(
-    '0.4124, 0.3576, 0.1805; 0.2126, 0.7152, 0.0722; 0.0193, 0.1192, 0.9505'
+# sRGB to XYZ from https://en.wikipedia.org/wiki/SRGB#Primaries:
+srgb2xyz = np.matrix(
+    [
+        [0.4124, 0.3576, 0.1805],
+        [0.2126, 0.7152, 0.0722],
+        [0.0193, 0.1192, 0.9505],
+    ]
 )
 
-# AOSP's transformation. This is why Android's color filters don't look right.
-# This is the CIECAM02 XYZ-to-RGB conversion matrix. It doesn't convert to LMS
-# at all. This is a bug in Android.
+# CIECAM02 transformation (https://en.wikipedia.org/wiki/LMS_color_space#Later_CIECAMs):
 #
-# https://github.com/LineageOS/android_frameworks_native/blob/lineage-22.2/services/surfaceflinger/Effects/Daltonizer.cpp
-# NB: AOSP's mat4's constructor accepts a column-major array!
-aosp_xyz2lms = np.matrix(
-    '0.7328,-0.7036, 0.0030; 0.4296, 1.6975, 0.0136; -0.1624, 0.0061, 0.9834'
-).T
+# Used by AOSP's filters. It converts to spectrally-sharpened LMS that doesn't
+# reflect actual cone sensitivity, yet the AOSP algorithm relies on it to infer
+# the sensitivity of the unaffected cones. This is why AOSP's filters don't
+# look right.
+ciecam02 = np.matrix(
+    [
+        [0.7328, 0.4296, -0.1624],
+        [-0.7036, 1.6975, 0.0061],
+        [0.003, 0.0136, 0.9834],
+    ]
+)
+
+
+# From http://brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+#
+# Effectively the same as the matrix given by Wikipedia, but with the advantage
+# that this matrix is given with an explicitly stated illuminant: D65.
+srgb_to_d65_xyz = np.matrix(
+    [
+        [0.4124564, 0.3575761, 0.1804375],
+        [0.2126729, 0.7151522, 0.0721750],
+        [0.0193339, 0.1191920, 0.9503041],
+    ]
+)
 
 # Hunt-Pointer-Estevez transformation (https://en.wikipedia.org/wiki/LMS_color_space#Hunt,_RLAB):
-hpe_xyz2lms = np.matrix(
-    # Equal-energy
-    # '0.38971, 0.68898, -0.07868; -0.22981, 1.18340, 0.04641; 0, 0, 1'
-    # D65-normalized
-    '0.4002, 0.7076, -0.0808; -0.2263, 1.1653, 0.0457; 0.0000, 0.0000, 0.9182'
+#
+# This version of the matrix is normalized to D65.
+hpe_d65_xyz_to_lms = np.matrix(
+    [
+        [0.4002, 0.7076, -0.0808],
+        [-0.2263, 1.1653, 0.0457],
+        [0.0000, 0.0000, 0.9182],
+    ]
 )
 
-aosp_rgb2lms = aosp_xyz2lms * rgb2xyz
-rgb2lms = hpe_xyz2lms * rgb2xyz
+aosp_rgb2lms = ciecam02 * srgb2xyz
+hpe_rgb2lms = hpe_d65_xyz_to_lms * srgb_to_d65_xyz
 
 print(
     json.dumps(
         {
-            'HPE': {
-                'rgb2lms': list(rgb2lms.T.flat),
-                'lms2rgb': list(rgb2lms.I.T.flat),
+            "HPE": {
+                "rgb2lms": list(hpe_rgb2lms.T.flat),
+                "lms2rgb": list(hpe_rgb2lms.I.T.flat),
             },
-            'AOSP': {
-                'rgb2lms': list(aosp_rgb2lms.T.flat),
-                'lms2rgb': list(aosp_rgb2lms.I.T.flat),
+            "AOSP": {
+                "rgb2lms": list(aosp_rgb2lms.T.flat),
+                "lms2rgb": list(aosp_rgb2lms.I.T.flat),
             },
         },
         indent=4,
