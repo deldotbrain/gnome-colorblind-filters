f37b89b8b0652bc0c101fcc5fb82089281ca036f
Amy Pennucci <apennucci@protonmail.com>
2025-07-22 22:51:12 +0200

    flake: nix package, etc.

--- flake.nix
+++ flake.nix
@@ -1,27 +1,53 @@
 {
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, self }:
+    let
+      inherit (nixpkgs) lib;
+
+      devDeps =
+        pkgs: with pkgs; [
+          just
+
+          # You know you've having a bad time when you need this for a
+          # *JavaScript* project:
+          #gdb
+
+          # Run from command line
+          gjs
+          clutter
+
+          # because there wasn't enough pain in this world, we created eslint
+          nodejs
+
+          # Building
+          gettext
+          glib.dev
+          gnumake
+          jq
+          xmlstarlet
+        ];
+
+      mkAttr = fn: lib.genAttrs lib.systems.flakeExposed (sys: fn nixpkgs.legacyPackages.${sys});
+    in
     {
-      devShells.x86_64-linux.default =
-        with nixpkgs.legacyPackages.x86_64-linux;
-        mkShell {
-          packages = [
-            just
+      packages = mkAttr (pkgs: rec {
+        default = gnome-colorblind-filters;
 
-            # Run from command line
-            gjs
-            clutter
-
-            # because there wasn't enough pain in this world, we created eslint
-            nodejs
-
-            # Building
-            gettext
-            glib.dev
-            gnumake
-            jq
-            xmlstarlet
-          ];
+        gnome-colorblind-filters = pkgs.callPackage ./package.nix {
+          srcRev = self.rev or self.dirtyRev;
         };
+
+        devEnv = pkgs.buildEnv {
+          name = "gnome-colorblind-filters-deps";
+          paths = devDeps pkgs;
+        };
+      });
+
+      devShells = mkAttr (pkgs: {
+        default = pkgs.mkShell {
+          name = "gnome-colorblind-filters-shell";
+          packages = devDeps pkgs;
+        };
+      });
     };
 }
---
+++ package.nix
@@ -0,0 +1,61 @@
+{
+  srcRev,
+
+  lib,
+  stdenvNoCC,
+
+  gettext,
+  glib,
+  zip,
+  unzip,
+  jq,
+  xmlstarlet,
+}:
+let
+  metadata = builtins.fromJSON (builtins.readFile ./metadata.json);
+  inherit (metadata) uuid description;
+in
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "gnome-shell-extension-colorblind-filters-advanced";
+  version = "g" + lib.substring 0 7 srcRev;
+  src = ./.;
+
+  nativeBuildInputs = [
+    gettext
+    glib
+    zip
+    unzip
+    jq
+    xmlstarlet
+  ];
+
+  # Not sure why this doesn't work, not really that worried about it.
+  # makeTargets = [ "zip" ];
+
+  buildPhase = ''
+    runHook preBuild
+
+    make zip
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/gnome-shell/extensions/${uuid}
+    unzip ${uuid}.zip -d $out/share/gnome-shell/extensions/${uuid}
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = builtins.head (lib.splitString "\n" description);
+    longDescription = description;
+    homepage = metadata.url;
+    license = lib.licenses.gpl3Only;
+    platforms = lib.platforms.linux;
+  };
+
+  passthru.extensionUuid = uuid;
+})
